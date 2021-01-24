36a94a25f301e53850fcb783bb77ab0e48300272
nickvazz <nickvazquezz@gmail.com>
2021-01-24 15:28:41 -0800

    add travis yml

---
+++ klickbrick/cli.py
@@ -0,0 +1,26 @@
+import argparse
+import sys
+
+
+def main(args=None):
+    if not args:
+        args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    
+    hello = subparsers.add_parser("hello", help='prints hello')
+    hello.add_argument('--name', type=str)
+
+    parsed_args = parser.parse_args(args)
+
+    if parsed_args.name:
+        output = f"Hello {parsed_args.name}"
+    else:
+        output = "Hello World"
+
+    print(output)
+    return output
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- tests/test_klickbrick.py
+++ tests/test_klickbrick.py
@@ -1,4 +1,16 @@
 from klickbrick import __version__
+from klickbrick.cli import main
+
+
+def test_hello_subcommand():
+    result = main(['hello'])
+
+    assert result == "Hello World"
+
+def test_hello_subcommand_with_name_flag():
+    result = main(['hello', '--name', 'bill'])
+
+    assert result == "Hello bill"
 
 
 def test_version():
